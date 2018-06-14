693abef4d10dd285724e26fa98035ba207e0a867
ffp01 <40261784+ffp01@users.noreply.github.com>
2018-06-14 11:24:37 +0200

    fach super

--- OT/Fach.java
+++ OT/Fach.java
@@ -8,7 +8,7 @@ public class Fach
     private String lehrer;
     private boolean schriftlich;
     private int kurs;
-    private List noten;
+    private List<Note> noten;
     public Fach(String pName, String pLehrer, int pKurs, boolean pSchriftlich)
     {
         noten = new List <Note> ();
@@ -22,19 +22,26 @@ public class Fach
     {
         noten.append(new Note (punktwert, schriftlich, quartal));
     }
-    
-    public int getNote (int punktwert, boolean schriftlich, int quartal)
+
+    public int getNote (boolean schriftlich, int quartal)
     {
         noten.toFirst();
-        if (noten.getContent().getSchriftlich() == schriftlich)
+        while(noten.getContent() != null)
         {
-            if (noten.getContent().getQuartal() == quartal)
+            if (noten.getContent().getSchriftlich() == schriftlich)
             {
-                
+                if (noten.getContent().getQuartal() == quartal)
+                {
+                    return noten.getContent().getPunktwert();
+                }
+                else{
+                    noten.next();
+                    return getNote(schriftlich, quartal);
+                }
             }
-            else{
-                
-            }
+            noten.next();
+            return getNote(schriftlich, quartal);
         }
+        return 0;
     }
 }
